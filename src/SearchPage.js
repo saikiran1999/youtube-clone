83fa26d15a5aa518da2d8d3d0fe5751f97466feb
kirannemala-rally <kiran.nemala@rallyhealth.com>
2021-09-24 16:57:59 +0530

    added video card in the search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './SearchPage.css'
 import TuneOutLinedIcon from "@material-ui/icons/TuneOutlined"
 import ChannelRow from "./ChannelRow"
+import VideoRow from './VideoRow'
 
 function SearchPage() {
     return (
@@ -20,6 +21,43 @@ function SearchPage() {
               description="You can Find awesome programming here! Also, expect programming tips and tricks that will take your coding skills..."
              />
             <hr />
+            <p className="videoCard__top">Latest from Clever Programmer</p>
+            <VideoRow 
+              views = "1.4M"
+               subs = "659k"
+               description= "Do You want free one hour Training on react.check this out...."
+               timeStamp="59 seconds ago"
+               channel="Clever Programmer"
+               title="Let Build a Youtube clone with React Js For Beginners"
+               image="https://i.ytimg.com/vi/NT299zIk2JY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqZPjIgBJ_NTf7TnAGCXmQQ5C85Q"
+            />
+            <VideoRow 
+              views = "1.4M"
+               subs = "659k"
+               description= "Do You want free one hour Training on react.check this out...."
+               timeStamp="59 seconds ago"
+               channel="Clever Programmer"
+               title="Let Build a Youtube clone with React Js For Beginners"
+               image="https://i.ytimg.com/vi/NT299zIk2JY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqZPjIgBJ_NTf7TnAGCXmQQ5C85Q"
+            />
+            <VideoRow 
+              views = "1.4M"
+               subs = "659k"
+               description= "Do You want free one hour Training on react.check this out...."
+               timeStamp="59 seconds ago"
+               channel="Clever Programmer"
+               title="Let Build a Youtube clone with React Js For Beginners"
+               image="https://i.ytimg.com/vi/NT299zIk2JY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqZPjIgBJ_NTf7TnAGCXmQQ5C85Q"
+            />
+            <VideoRow 
+              views = "1.4M"
+               subs = "659k"
+               description= "Do You want free one hour Training on react.check this out...."
+               timeStamp="59 seconds ago"
+               channel="Clever Programmer"
+               title="Let Build a Youtube clone with React Js For Beginners"
+               image="https://i.ytimg.com/vi/NT299zIk2JY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqZPjIgBJ_NTf7TnAGCXmQQ5C85Q"
+            />
         </div>
     )
 }
